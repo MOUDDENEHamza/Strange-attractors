38a7e15caf02e197dadd931abfd891d8f47961cf
Apple <apple@apples-macbook-10.home>
2018-11-18 19:40:19 +0100

    input library

--- include/input.h
+++ include/input.h
@@ -1,11 +1,7 @@
 #ifndef _INPUT_H_
 #define _INPUT_H_
 
-#define sigma 10
-#define rho 28
-#define beta 8.0 / 3.0
-
-
+/*input : coordonnees x, y, z*/
 void coordinates(double position[]){
 	printf("entrez votre coordonnée x : ");
         scanf("%lf", &position[0]);
@@ -13,6 +9,28 @@ void coordinates(double position[]){
         scanf("%lf", &position[1]);
         printf("entrez votre coordonnée z : ");
         scanf("%lf", &position[2]);
+}
+
+/*input : constantes σ, ρ, β*/ 
+void  constants(double cste[]){
+	printf("entrez votre σ : ");
+	scanf("%lf", &cste[0]);
+        printf("entrez votre ρ : ");
+        scanf("%lf", &cste[1]);
+        printf("entrez votre β : ");
+        scanf("%lf", &cste[2]);
+}
+
+/*input : incrément*/
+void increment(double dt){
+        printf("entrez votre incrément dt : ");        
+	scanf("%lf", &dt);
+}
+
+/*input : temps d'arret Tmax*/
+void break_time(double tmax){
+        printf("entrez votre temps d'arret Tmax : ");
+	scanf("%lf", &tmax);
 }
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]){
 	/*initialiser toutes les variables*/
 	double position[3];
 	double speed[3];
+	double cste[3];
 	double dt;
 	double tmax;
         double i = 0;
@@ -15,34 +16,36 @@ int main(int argc, char *argv[]){
 	
 	f = fopen("lorenz.dat", "w+");
 	fputs("temps		x		y		z\n", f);
-	
-	/*input: coordonnées initiales*/
+
+	/*input : coordonnées initiales*/
 	coordinates(position);
-        fprintf(f, "%lf	%lf	%lf	%lf\n", i, position[0], position[1], position[2]);
+
+	/*input : constantes σ, ρ, β*/
+	constants(cste);
 	
 	/*calcul et affichage de la vitesse dx dy dz*/
-	speed[0] = sigma * (position[1] - position[0]);
-        speed[1] = position[0] * (rho - position[2]) -position[1];
-        speed[2] = position[0] * position[1] - beta * position[2];
+	speed[0] = cste[0] * (position[1] - position[0]);
+        speed[1] = position[0] * (cste[1] - position[2]) -position[1];
+        speed[2] = position[0] * position[1] - cste[2] * position[2];
         printf("dx = %lf \n", speed[0]);
         printf("dy = %lf \n", speed[1]);
         printf("dz = %lf \n", speed[2]);
 	
 	/*input : incrément*/
-	printf("entrez votre incrément dt : ");
-        scanf("%lf", &dt);
-	
+	increment(dt);
+	printf("%lf \n", dt);
 	/*input : temps d'arret Tmax*/
-	printf("entrez votre temps d'arret Tmax : ");
-        scanf("%lf", &tmax);
-	
+	break_time(tmax);
+	printf("%lf \n", tmax);	
+
 	/*calcul et affichage de la position x y z entre t = 0 et Tmax*/
-        /*for(i = dt; i <= tmax; i += dt){*/
+        /*for(i = dt; i <= tmax; i += dt){*/;
+	fprintf(f, "%lf %lf     %lf     %lf\n", i, position[0], position[1], position[2]);
 	i = dt;
 	do{
-                xt = position[0] + (sigma * ( position[1] - position[0])) * dt;
-                yt = position[1] + (position[0] * (rho - position[2]) - position[1]) * dt;
-                zt = position[2] + (position[0] * position[1] - beta * position[2]) * dt;
+                xt = position[0] + (cste[0] * ( position[1] - position[0])) * dt;
+                yt = position[1] + (position[0] * (cste[1] - position[2]) - position[1]) * dt;
+                zt = position[2] + (position[0] * position[1] - cste[2] * position[2]) * dt;
 		position[0] = xt;
     		position[1] = yt;
     		position[2] = zt;
