167211788caf11803d7c621553781c9014ecf3a2
hamza <hz.mouddene@gmail.com>
2018-11-23 00:00:40 +0100

    input library done

--- include/input.h
+++ include/input.h
@@ -3,34 +3,51 @@
 
 /*input : coordonnees x, y, z*/
 void coordinates(double position[]){
-	printf("entrez votre coordonnée x : ");
+	printf("entrez la coordonnée x : ");
         scanf("%lf", &position[0]);
-        printf("entrez votre coordonnée y : ");
+        printf("entrez la coordonnée y : ");
         scanf("%lf", &position[1]);
-        printf("entrez votre coordonnée z : ");
+        printf("entrez la coordonnée z : ");
         scanf("%lf", &position[2]);
 }
 
 /*input : constantes σ, ρ, β*/ 
-void  constants(double cste[]){
-	printf("entrez votre σ : ");
-	scanf("%lf", &cste[0]);
-        printf("entrez votre ρ : ");
-        scanf("%lf", &cste[1]);
-        printf("entrez votre β : ");
-        scanf("%lf", &cste[2]);
+void  constants(double *sigma, double *rho, double *beta){
+	printf("entrez σ : ");
+	scanf("%lf", sigma);
+        printf("entrez ρ : ");
+        scanf("%lf", rho);
+        printf("entrez β : ");
+        scanf("%lf", beta);
 }
 
 /*input : incrément*/
-void increment(double dt){
-        printf("entrez votre incrément dt : ");        
-	scanf("%lf", &dt);
+void increment(double *dt){
+        while(1){
+		printf("entrez l'incrément dt : ");
+                scanf("%lf", dt);
+		/*erreur : l'incrément est nul ou négatif*/
+		if(*dt <= 0){
+			printf("l'incrément est incorrect, réessayez encore.\n");
+			continue;
+		}
+		return;
+	}
+	
 }
 
 /*input : temps d'arret Tmax*/
-void break_time(double tmax){
-        printf("entrez votre temps d'arret Tmax : ");
-	scanf("%lf", &tmax);
+void break_time(double *tmax, double *dt){
+	while(1){
+        	printf("entrez le temps d'arret Tmax : ");
+		scanf("%lf", tmax);
+		/*erreur : le temps d'arret est inférieur a dt*/
+		if(*tmax <= *dt){
+			printf("le temps d'arret est incorrect, réessayer encore.\n");
+                	continue;
+                }
+        	return;
+	}
 }
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]){
 	/*initialiser toutes les variables*/
 	double position[3];
 	double speed[3];
-	double cste[3];
+	double sigma, rho, beta;
 	double dt;
 	double tmax;
         double i = 0;
@@ -21,31 +21,30 @@ int main(int argc, char *argv[]){
 	coordinates(position);
 
 	/*input : constantes σ, ρ, β*/
-	constants(cste);
+	constants(&sigma, &rho, &beta);
 	
 	/*calcul et affichage de la vitesse dx dy dz*/
-	speed[0] = cste[0] * (position[1] - position[0]);
-        speed[1] = position[0] * (cste[1] - position[2]) -position[1];
-        speed[2] = position[0] * position[1] - cste[2] * position[2];
+	speed[0] = sigma * (position[1] - position[0]);
+        speed[1] = position[0] * (rho - position[2]) -position[1];
+        speed[2] = position[0] * position[1] - beta * position[2];
         printf("dx = %lf \n", speed[0]);
         printf("dy = %lf \n", speed[1]);
         printf("dz = %lf \n", speed[2]);
 	
 	/*input : incrément*/
-	increment(dt);
-	printf("%lf \n", dt);
+	increment(&dt);
+
 	/*input : temps d'arret Tmax*/
-	break_time(tmax);
-	printf("%lf \n", tmax);	
+	break_time(&tmax, &dt);
 
 	/*calcul et affichage de la position x y z entre t = 0 et Tmax*/
         /*for(i = dt; i <= tmax; i += dt){*/;
-	fprintf(f, "%lf %lf     %lf     %lf\n", i, position[0], position[1], position[2]);
+	fprintf(f, "%lf	%lf	%lf	%lf\n", i, position[0], position[1], position[2]);
 	i = dt;
 	do{
-                xt = position[0] + (cste[0] * ( position[1] - position[0])) * dt;
-                yt = position[1] + (position[0] * (cste[1] - position[2]) - position[1]) * dt;
-                zt = position[2] + (position[0] * position[1] - cste[2] * position[2]) * dt;
+                xt = position[0] + (sigma * ( position[1] - position[0])) * dt;
+                yt = position[1] + (position[0] * (rho - position[2]) - position[1]) * dt;
+                zt = position[2] + (position[0] * position[1] - beta * position[2]) * dt;
 		position[0] = xt;
     		position[1] = yt;
     		position[2] = zt;
