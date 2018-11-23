14c5ef721576b9b83d3eb7c200e58426b38eee51
hamza <hz.mouddene@gmail.com>
2018-11-23 02:13:45 +0100

    Makefile update input.h create input.c

--- include/input.h
+++ include/input.h
@@ -2,52 +2,15 @@
 #define _INPUT_H_
 
 /*input : coordonnees x, y, z*/
-void coordinates(double position[]){
-	printf("entrez la coordonnée x : ");
-        scanf("%lf", &position[0]);
-        printf("entrez la coordonnée y : ");
-        scanf("%lf", &position[1]);
-        printf("entrez la coordonnée z : ");
-        scanf("%lf", &position[2]);
-}
+void coordinates(double position[]);
 
 /*input : constantes σ, ρ, β*/ 
-void  constants(double *sigma, double *rho, double *beta){
-	printf("entrez σ : ");
-	scanf("%lf", sigma);
-        printf("entrez ρ : ");
-        scanf("%lf", rho);
-        printf("entrez β : ");
-        scanf("%lf", beta);
-}
+void  constants(double *sigma, double *rho, double *beta);
 
 /*input : incrément*/
-void increment(double *dt){
-        while(1){
-		printf("entrez l'incrément dt : ");
-                scanf("%lf", dt);
-		/*erreur : l'incrément est nul ou négatif*/
-		if(*dt <= 0){
-			printf("l'incrément est incorrect, réessayez encore.\n");
-			continue;
-		}
-		return;
-	}
-	
-}
+void increment(double *dt);
 
 /*input : temps d'arret Tmax*/
-void break_time(double *tmax, double *dt){
-	while(1){
-        	printf("entrez le temps d'arret Tmax : ");
-		scanf("%lf", tmax);
-		/*erreur : le temps d'arret est inférieur a dt*/
-		if(*tmax <= *dt){
-			printf("le temps d'arret est incorrect, réessayer encore.\n");
-                	continue;
-                }
-        	return;
-	}
-}
+void break_time(double *tmax, double *dt);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "../include/input.h"
+#include "input.h"
 
 int main(int argc, char *argv[]){
 	/*initialiser toutes les variables*/
